a88a23c7af234951dd73237fe676787ad68eee9b
Al amin <alaminmridha2004@gmail.com>
2025-10-17 15:42:02 +0600

    Project Initialize

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="h-screen flex justify-center items-center text-4xl sm:text-5xl md:text-6xl font-medium text-[#FFBF00]">
+      Dream - Team
+    </div>
+  );
+}
