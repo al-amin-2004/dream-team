bbc04ab91a18cbb08b61b49b71e2aacfc7a56552
Al amin <alaminmridha2004@gmail.com>
2025-10-24 17:36:23 +0600

    leaderboard stage add for pc

--- src/app/(branches)/dream-future/(pagas)/leaderboard/page.tsx
+++ src/app/(branches)/dream-future/(pagas)/leaderboard/page.tsx
@@ -1,17 +1,44 @@
 import { DiamondIcon } from "@/icons";
+import Image from "next/image";
+import Box3D from "../../_components/ui/Box";
 
 const leaderboard = async () => {
   return (
-    <main className="px-2.5 md:px-0 mb-10">
-      <div className="container">
-        
+    <>
+      <div
+        className="absolute -z-50 h-[50rem] w-full top-0 -mt-5 md:-mt-10"
+        style={{
+          background:
+            "radial-gradient(ellipse 80% 60% at 50% 0%, rgba(251, 191, 36, 0.25), transparent 70%)",
+        }}
+      />
 
+      <div className="container">
+        <section>
+          <div className="flex justify-center items-center gap-[7rem] h-[calc(100vh-20rem)]">
+            <Box3D
+              position={2}
+              name={"AL AMIN"}
+              img={"/logos/dream-future-logo-white.png"}
+            />
+            <Box3D
+              position={1}
+              name={"MD MONIRUL ISLAM"}
+              img={"/logos/dream-future-logo-white.png"}
+            />
+            <Box3D
+              position={3}
+              name={"ABRARUL HASAN SAJIB"}
+              img={"/logos/dream-future-logo-white.png"}
+            />
+          </div>
+        </section>
 
         <table className="w-full">
           <thead>
             <tr className="border-b-2 text-primary">
-              <th className="p-2.5 text-left w-20">Rank</th>
-              <th className="p-2.5 text-left w-auto">Name</th>
+              <th className="p-2.5 text-left w-30">Rank</th>
+              <th className="p-2.5 text-left w-auto">User Name</th>
               <th className="p-2.5 text-right w-20">Stone</th>
             </tr>
           </thead>
@@ -19,12 +46,27 @@ const leaderboard = async () => {
             {Array.from({ length: 50 }).map((_, idx) => (
               <tr
                 key={idx}
-                className="even:bg-white/20 [&>td]:first:rounded-tl-sm [&>td]:first:rounded-bl-sm [&>td]:last:rounded-tr-sm [&>td]:last:rounded-br-sm text-sm md:text-base rounded-2xl overflow-hidden"
+                className="even:bg-[#1d2130] [&>td]:first:rounded-tl-sm [&>td]:first:rounded-bl-sm [&>td]:last:rounded-tr-sm [&>td]:last:rounded-br-sm text-sm md:text-base rounded-2xl overflow-hidden"
               >
-                <td className="p-2.5">{idx + 1}</td>
-                <td className="p-2.5">Al amin</td>
-                <td className="p-2.5 text-right">
-                  <span className="inline-flex px-2 items-center justify-center gap-1 rounded-full bg-gray-200/20">
+                <td className="p-2">{idx + 1}</td>
+                <td className="p-2">
+                  <span className="inline-flex items-center gap-3">
+                    <Image
+                      src={
+                        false
+                          ? `https://drive.google.com/uc?export=view&id=${""}`
+                          : "/logos/dream-future-logo-white.png"
+                      }
+                      width={500}
+                      height={500}
+                      alt="profile Image"
+                      className="size-7 md:size-9 rounded-full overflow-hidden"
+                    />
+                    <p>Al amin</p>
+                  </span>
+                </td>
+                <td className="p-2 text-right">
+                  <span className="inline-flex items-center justify-center gap-1 px-2 rounded-full bg-gray-200/20">
                     <DiamondIcon className="size-4" />
                     <p>00</p>
                   </span>
@@ -34,7 +76,7 @@ const leaderboard = async () => {
           </tbody>
         </table>
       </div>
-    </main>
+    </>
   );
 };
 export default leaderboard;
--- src/app/(branches)/dream-future/_components/ui/Box.tsx
+++ src/app/(branches)/dream-future/_components/ui/Box.tsx
@@ -1,5 +1,5 @@
+import { CSSProperties, FC } from "react";
 import Image from "next/image";
-import { FC } from "react";
 
 interface Box3DProps {
   position: number;
@@ -7,28 +7,34 @@ interface Box3DProps {
   img?: string;
 }
 
+type CSSVariables = CSSProperties & { "--size"?: string };
+
+const getBoxSize = (position: number) => {
+  switch (position) {
+    case 1:
+      return "21rem";
+    case 2:
+      return "17rem";
+    case 3:
+      return "15rem";
+    default:
+      return "10rem";
+  }
+};
+
 const Box3D: FC<Box3DProps> = ({ position, name, img }) => {
+  const style: CSSVariables = {
+    "--size": getBoxSize(position),
+  };
+
   return (
-    <div
-      style={{
-        "--size": `${
-          position === 1
-            ? "21rem"
-            : position === 2
-            ? "17rem"
-            : position === 3
-            ? "15rem"
-            : "12rem"
-        }`,
-      }}
-    >
+    <div style={style}>
       <div
         className={`relative size-[var(--size)] -rotate-x-[15deg] transform-3d perspective-midrange`}
       >
-        <div className="size-7/12 mx-auto bg-white/40 overflow-hidden rounded-2xl -mb-3">
+        <div className="size-7/12 mx-auto bg-white/40 overflow-hidden rounded-lg -mb-3">
           <Image
-            // src={`https://drive.google.com/uc?export=view&id=${img}`}
-            src={""}
+            src={img ? img : "/logos/dream-future-logo-white.png"}
             width={500}
             height={500}
             alt="leaderboard Image"
@@ -38,7 +44,7 @@ const Box3D: FC<Box3DProps> = ({ position, name, img }) => {
 
         {/* top side  */}
         <div
-          className="topside absolute size-full transform-3d  bg-linear-to-b from-[#fde04c] dark:from-[#161A29] to-[transparent]"
+          className="topside absolute size-full transform-3d bg-linear-to-b from-[#161A29] to-[transparent]"
           style={{
             transform: "rotateX(95deg) translateZ(calc(var(--size) * 0.45))",
           }}
@@ -47,11 +53,11 @@ const Box3D: FC<Box3DProps> = ({ position, name, img }) => {
         {/* Around side  */}
         <div className="size-full transform-3d">
           <div
-            className="absolute top-0 left-0 size-full bg-linear-to-b from-[#fde04c] dark:from-[#161A29] via-[transparent] to-[transparent]"
+            className="absolute top-0 left-0 size-full bg-linear-to-b from-[#161A29] via-[transparent] to-[transparent]"
             style={{ transform: "translateZ(calc(var(--size) * 0.48))" }}
           >
             <h1
-              className={`text-center text-text dark:text-dark-text md:font-medium border-b border-slate-300 py-2 ${
+              className={`text-center md:font-medium border-b border-slate-300 py-2 ${
                 position === 1
                   ? "text-3xl"
                   : position === 2
@@ -65,14 +71,14 @@ const Box3D: FC<Box3DProps> = ({ position, name, img }) => {
             <div className="flex justify-center items-center">
               {position === 1 ? (
                 <Image
-                  src="/images/leaderboard-1.png"
+                  src="/leaderboard-1.png"
                   width={500}
                   height={500}
                   alt="leaderboard Number Image"
                   className="w-6/12 p-2"
                 />
               ) : (
-                <b className="text-7xl text-dark-text-secondary italic">
+                <b className="text-7xl italic">
                   {position === 2 ? `${position}nd` : `${position}rd`}
                 </b>
               )}
