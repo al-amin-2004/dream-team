5d68e17afe5271ccdeaa5f341728694090827263
Al amin <alaminmridha2004@gmail.com>
2025-10-27 17:33:44 +0600

    Add metatag

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,25 @@
+import Link from "next/link";
+import { Button } from "./(branches)/dream-future/_components/ui/Button";
+
 export default function Home() {
+  const divitions = [
+    { label: "Dream Future", link: "dream-future" },
+    { label: "Dream Shop", link: "dream-shop" },
+    { label: "Dream Foundation", link: "dream-foundation" },
+  ];
   return (
-    <div className="h-screen flex justify-center items-center text-4xl sm:text-5xl md:text-6xl font-medium text-[#FFBF00]">
-      Dream - Team
+    <div className="h-screen flex flex-col justify-center items-center gap-y-12">
+      <h1 className="text-4xl sm:text-5xl md:text-6xl font-medium text-primary">
+        Dream - Team
+      </h1>
+
+      <div className="flex gap-5">
+        {divitions.map(({ label, link }, idx) => (
+          <Link key={idx} href={link}>
+            <Button variant={"outline"} className="text-white">{label}</Button>
+          </Link>
+        ))}
+      </div>
     </div>
   );
 }
