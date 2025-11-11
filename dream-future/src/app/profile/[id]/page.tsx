39cd1b4883af467b9c695a8c2002408414800150
Al amin <alaminmridha2004@gmail.com>
2025-11-11 17:31:19 +0600

    feat(profile): add total balance display and improve layout for profile details

--- dream-future/src/app/profile/[id]/page.tsx
+++ dream-future/src/app/profile/[id]/page.tsx
@@ -22,10 +22,43 @@ const Profile = () => {
           />
         </div>
 
-        <div className="border-2 p-6 rounded-xl flex-3">
-          <div className="flex justify-between py-3 border-b">
-            <h1 className="text-xl font-semibold">Bio & others details</h1>
-            <span className="size-2.5 bg-green-500 rounded-full" />
+        {/* Top right side */}
+        <div className="flex-3 flex flex-col gap-8 ">
+          <div className="border-2 p-6 rounded-xl">
+            <div className="flex justify-between py-3 border-b">
+              <h1 className="text-xl font-semibold">Balance quiry</h1>
+              <span className="size-2.5 bg-green-500 rounded-full" />
+            </div>
+
+            <div className="w-full flex gap-4 mt-6">
+              <div className="p-3 space-y-2.5 rounded-xl flex-1 bg-yellow-500/30">
+                <h3 className="text-sm text-yellow-500">Total Diposite</h3>
+                <p className="text-2xl font-bold">$ 1400</p>
+              </div>
+              <div className="p-3 space-y-2.5 rounded-xl flex-1 bg-green-500/20">
+                <h3 className="text-sm text-green-500">Profit</h3>
+                <p className="text-2xl font-bold">$ 400</p>
+              </div>
+              <div className="p-3 space-y-2.5 rounded-xl flex-1 bg-blue-500/20">
+                <h3 className="text-sm text-blue-500">Total Balance</h3>
+                <p className="text-2xl font-bold">$ 1800</p>
+              </div>
+            </div>
+          </div>
+
+          <div className="border-2 p-6 rounded-xl flex-3">
+            <h1 className="text-xl font-semibold py-3 border-b">
+              Bio & others details
+            </h1>
+
+            <div>
+              <h1>Email: example00@gmail.com</h1>
+              <h1>Phone: +880 1609 305655</h1>
+              <h1>Phone: +880 1609 305655</h1>
+              <h1>Nationality: Bangladeshi</h1>
+              <h1>Blood: AB+</h1>
+              <h1>Role: Member</h1>
+            </div>
           </div>
         </div>
       </div>
