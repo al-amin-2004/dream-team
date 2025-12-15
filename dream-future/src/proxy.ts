d8dab6967a9bcd36a96d5377e45f6940e09162ed
Al amin <alaminmridha2004@gmail.com>
2025-12-15 22:43:45 +0600

    feat: update proxy logic to handle login and registration redirects

--- dream-future/src/proxy.ts
+++ dream-future/src/proxy.ts
@@ -1,18 +1,21 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
-// This function can be marked `async` if using `await` inside
 export function proxy(req: NextRequest) {
   const token = req.cookies.get("auth_token")?.value;
+  const { pathname } = req.nextUrl;
 
   if (!token && req.nextUrl.pathname.startsWith("/profile")) {
     return NextResponse.redirect(new URL("/login", req.url));
   }
 
+  if (token && (pathname === "/login" || pathname === "/register")) {
+    return NextResponse.redirect(new URL("/", req.url));
+  }
+
   return NextResponse.next();
 }
 
-// See "Matching Paths" below to learn more
 export const config = {
-  matcher: "/profile/:path*", // profile route protect
+  matcher: ["/profile/:path*", "/login", "/register"],
 };
