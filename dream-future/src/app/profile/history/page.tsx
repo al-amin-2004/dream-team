d72b68b5593feb97d7013e231cd992eaae0f9cca
Al amin <alaminmridha2004@gmail.com>
2025-12-16 11:02:07 +0600

    fix: remove unnecessary background color from history grid component

--- dream-future/src/app/profile/history/page.tsx
+++ dream-future/src/app/profile/history/page.tsx
@@ -100,7 +100,7 @@ const History = () => {
             }`}
           >
             <div
-              className={`w-full rounded-md p-4 min-w-[350px] bg-[#1e1f21] grid grid-cols-5 place-items-center text-lg font-medium border border-emerald-600 sticky top-0 ${
+              className={`w-full rounded-md p-4 min-w-[350px] grid grid-cols-5 place-items-center text-lg font-medium border border-emerald-600 sticky top-0 ${
                 isGrid && "hidden"
               }`}
             >
